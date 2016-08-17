22950a44bac26209a9a8eb26cb046703d3b241b8
Alex Morega <alex@grep.ro>
2016-08-17 22:48:12 +0300

    script to install in a shared account

---
+++ bin/shared.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function install_dotfiles {
+    rm -rf ~/.dotfiles
+    ln -s $DOTFILES ~/.dotfiles
+}
+
+function install_vim {
+    rm -rf ~/.vim ~/.vimrc ~/.viminfo
+    ln -s $DOTFILES/vim ~/.vim
+    ln -s ~/.vim/vimrc ~/.vimrc
+}
+
+
+function install_bash {
+    rm -f ~/.bashrc
+    ln -s $DOTFILES/bash/bashrc ~/.bashrc
+}
+
+
+install_dotfiles
+install_vim
+install_bash
